f65ecba3adf643dc1d57152195c720deedd261f0
Kiryl Charnyshou <Chawert@protonmail.com>
2023-07-10 07:18:32 +0200

    Add admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  root 'pages#home'
+  devise_for :admin, controllers: {
+    registrations: 'admin/registrations',
+    sessions: 'admin/sessions'
+  }
+  
   devise_for :users, controllers: {
     confirmations: 'users/confirmations',
     passwords: 'users/passwords',
@@ -7,4 +11,6 @@ Rails.application.routes.draw do
     sessions: 'users/sessions',
     unlocks: 'users/unlocks'
   }
+
+  root 'pages#home'
 end
