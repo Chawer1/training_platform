77e1080bf37b07c3351cbe873eac9beda2e79dd0
Kiryl Charnyshou <Chawert@protonmail.com>
2023-11-13 19:01:58 +0100

    Fix schema names

--- db/schema.rb
+++ db/schema.rb
@@ -11,30 +11,21 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_07_26_031632) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
     t.string "resource_type"
-    t.integer "resource_id"
+    t.bigint "resource_id"
     t.string "author_type"
-    t.integer "author_id"
+    t.bigint "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author"
     t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
     t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource"
-  end
-
-  create_table "admin_users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_admin_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
   create_table "admins", force: :cascade do |t|
@@ -55,7 +46,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_26_031632) do
     t.integer "duration"
     t.string "difficulty"
     t.decimal "price"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "creator_id"
@@ -63,8 +54,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_26_031632) do
   end
 
   create_table "enrollments", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "course_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "course_id", null: false
     t.date "enrollment_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -75,15 +66,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_26_031632) do
   create_table "lessons", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.integer "course_id", null: false
+    t.bigint "course_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["course_id"], name: "index_lessons_on_course_id"
   end
 
   create_table "submissions", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "task_id", null: false
+    t.bigint "user_id", null: false
+    t.bigint "task_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["task_id"], name: "index_submissions_on_task_id"
@@ -96,7 +87,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_26_031632) do
     t.string "materials"
     t.datetime "date_created"
     t.string "status"
-    t.integer "lesson_id", null: false
+    t.bigint "lesson_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["lesson_id"], name: "index_tasks_on_lesson_id"
